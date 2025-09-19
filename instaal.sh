508ea41bef452b7cc6f5806dbf414a80ec8a9bff
DrKingShultz <toufikgb@hotmail.com>
2025-09-19 12:51:15 +0100

    Delete instaal.sh

--- instaal.sh
+++
@@ -1,7 +0,0 @@
-sudo yum remove docker docker-common docker-selinux docker-engine
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker $USER
